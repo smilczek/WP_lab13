95be838fe008ecf78361eb525c34e9df4cb68275
smilczek <93130675+smilczek@users.noreply.github.com>
2022-01-26 23:15:22 +0100

    Update test_main2.py

--- test/temp/test_main2.py
+++ test/temp/test_main2.py
@@ -1,1 +1,10 @@
-# not empty
+from temp import main2
+
+def test_five_times():
+  assert main2.five_times('#') == '#####'
+  assert main2.five_times('some text') == 'some textsome textsome textsome textsome text'
+  
+def test_what_is():
+  assert main2.what_is('whatever') == 'garbage'
+  assert main2.what_is('gold') == 'garbage'
+  assert main2.what_is('garbage') == 'garbage'
