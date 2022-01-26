3e03658d207a3dd10c60add223b134d742e86f21
smilczek <93130675+smilczek@users.noreply.github.com>
2022-01-26 23:09:19 +0100

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,1 +1,9 @@
-# not empty
+import main
+
+def test_factorial():
+  assert main.factorial(5) == 120
+  assert main.factorial(4) == 24
+  
+def test_sum():
+  assert main.sum(1, 1) == 2
+  assert main.sum(1, 0) == 1
